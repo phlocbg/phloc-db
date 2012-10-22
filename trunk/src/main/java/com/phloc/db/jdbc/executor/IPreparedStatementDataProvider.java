37b4595ec7210b79900bdd511f8bd877f195bb49
ph@phloc.com <ph@phloc.com@a43f803d-45c2-174a-4524-3f8b5083f414>
2012-10-22 13:02:20 +0000

    Initial version as OSS

---
+++ trunk/src/main/java/com/phloc/db/jdbc/executor/IPreparedStatementDataProvider.java
@@ -0,0 +1,40 @@
+/**
+ * Copyright (C) 2006-2012 phloc systems
+ * http://www.phloc.com
+ * office[at]phloc[dot]com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.phloc.db.jdbc.executor;
+
+import java.util.List;
+
+import javax.annotation.Nonnegative;
+import javax.annotation.Nonnull;
+
+public interface IPreparedStatementDataProvider
+{
+  /**
+   * @return The number of parameters provided by this instance.
+   */
+  @Nonnegative
+  int getValueCount ();
+
+  /**
+   * @return A non-<code>null</code>, unmodifiable list of values. The length of
+   *         the returned list must match the result of {@link #getValueCount()}
+   *         .
+   */
+  @Nonnull
+  List <Object> getObjectValues ();
+}
