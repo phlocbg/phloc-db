242a5e6b942b15e357b3fe34901575558a456551
ph@phloc.com <ph@phloc.com@a43f803d-45c2-174a-4524-3f8b5083f414>
2013-05-01 09:28:07 +0000

    Package name changes

--- trunk/phloc-db-jdbc/src/main/java/com/phloc/db/jdbc/db2/AbstractDB2Connector.java
+++ trunk/phloc-db-jdbc/src/main/java/com/phloc/db/jdbc/db2/AbstractDB2Connector.java
@@ -15,7 +15,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.phloc.db.db2;
+package com.phloc.db.jdbc.db2;
 
 import javax.annotation.Nonnull;
 
