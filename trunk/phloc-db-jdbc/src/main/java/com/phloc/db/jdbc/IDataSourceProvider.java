9c46a71c7f0d9f44629c5d5b36f61dec9c7fce90
ph@phloc.com <ph@phloc.com@a43f803d-45c2-174a-4524-3f8b5083f414>
2013-05-01 09:23:45 +0000

    Initial copy

---
+++ trunk/phloc-db-jdbc/src/main/java/com/phloc/db/jdbc/IDataSourceProvider.java
@@ -0,0 +1,37 @@
+/**
+ * Copyright (C) 2006-2013 phloc systems
+ * http://www.phloc.com
+ * office[at]phloc[dot]com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.phloc.db.jdbc;
+
+import javax.sql.DataSource;
+
+/**
+ * A simple provider interface for {@link DataSource} objects.
+ * 
+ * @author Philip Helger
+ */
+public interface IDataSourceProvider
+{
+  /**
+   * Retrieve a data source. It is up to the implementation whether the same
+   * data source is returned or whether a new data source object is created all
+   * the time.
+   * 
+   * @return A non-<code>null</code> data source.
+   */
+  DataSource getDataSource ();
+}
---
+++ trunk/phloc-db-jdbc/src/main/java/com/phloc/db/jpa/eclipselink/JPASessionCustomizer.java
@@ -0,0 +1,66 @@
+/**
+ * Copyright (C) 2006-2013 phloc systems
+ * http://www.phloc.com
+ * office[at]phloc[dot]com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.phloc.db.jpa.eclipselink;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+import org.eclipse.persistence.config.SessionCustomizer;
+import org.eclipse.persistence.logging.SessionLog;
+import org.eclipse.persistence.sessions.Session;
+
+import com.phloc.commons.CGlobal;
+
+/**
+ * Class for customizing JPA sessions.<br>
+ * Set the class name in the property
+ * <code>eclipselink.session.customizer</code><br>
+ * Should have a public no-argument ctor
+ * 
+ * @author Philip Helger
+ */
+@Deprecated
+public final class JPASessionCustomizer implements SessionCustomizer
+{
+  private static final AtomicInteger s_aLogLevel = new AtomicInteger (CGlobal.ILLEGAL_UINT);
+
+  public JPASessionCustomizer ()
+  {}
+
+  public static void setGlobalLogLevel (final int nLogLevel)
+  {
+    if (nLogLevel >= SessionLog.ALL && nLogLevel <= SessionLog.OFF)
+      s_aLogLevel.set (nLogLevel);
+  }
+
+  public static int getGlobalLogLevel ()
+  {
+    return s_aLogLevel.get ();
+  }
+
+  public void customize (final Session aSession) throws Exception
+  {
+    final int nLogLevel = getGlobalLogLevel ();
+    if (nLogLevel != CGlobal.ILLEGAL_UINT)
+    {
+      // create a custom logger and assign it to the session
+      final SessionLog aCustomLogger = new JPALogger ();
+      aCustomLogger.setLevel (nLogLevel);
+      aSession.setSessionLog (aCustomLogger);
+    }
+  }
+}
