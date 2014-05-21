9e7f4161951eaa8bbbc769bc4553245d20d5a4ed
ph@phloc.com <ph@phloc.com@a43f803d-45c2-174a-4524-3f8b5083f414>
2014-05-21 09:26:31 +0000

    [maven-release-plugin]  copy for tag phloc-db-3.1.2

---
+++ tags/phloc-db-3.1.2/src/main/java/com/phloc/db/jpa/eclipselink/EclipseLinkLogger.java
@@ -0,0 +1,74 @@
+/**
+ * Copyright (C) 2006-2014 phloc systems
+ * http://www.phloc.com
+ * office[at]phloc[dot]com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.phloc.db.jpa.eclipselink;
+
+import java.util.List;
+
+import org.eclipse.persistence.logging.AbstractSessionLog;
+import org.eclipse.persistence.logging.SessionLog;
+import org.eclipse.persistence.logging.SessionLogEntry;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.phloc.commons.CGlobal;
+import com.phloc.commons.GlobalDebug;
+import com.phloc.commons.string.StringHelper;
+
+/**
+ * A logging adapter that can be hooked into JPA and forwards all logging
+ * requests to phloc logging.
+ * 
+ * @author Philip Helger
+ */
+public final class EclipseLinkLogger extends AbstractSessionLog
+{
+  private static final Logger s_aLogger = LoggerFactory.getLogger (EclipseLinkLogger.class);
+
+  @Override
+  public void log (final SessionLogEntry aSessionLogEntry)
+  {
+    final int nLogLevel = aSessionLogEntry.getLevel ();
+    if (!shouldLog (nLogLevel))
+      return;
+
+    // JPA uses the System property for adding line breaks
+    final List <String> aMsgLines = StringHelper.getExploded (CGlobal.LINE_SEPARATOR, formatMessage (aSessionLogEntry));
+    final int nMaxIndex = aMsgLines.size ();
+    for (int i = 0; i < nMaxIndex; ++i)
+    {
+      final String sMsg = aMsgLines.get (i);
+      final Throwable t = i == nMaxIndex - 1 ? aSessionLogEntry.getException () : null;
+      if (nLogLevel >= SessionLog.SEVERE)
+        s_aLogger.error (sMsg, t);
+      else
+        if (nLogLevel >= SessionLog.WARNING)
+          s_aLogger.warn (sMsg, t);
+        else
+          if (nLogLevel >= SessionLog.CONFIG || GlobalDebug.isDebugMode ())
+          {
+            if (s_aLogger.isInfoEnabled ())
+              s_aLogger.info (sMsg, t);
+          }
+          else
+          {
+            if (s_aLogger.isDebugEnabled ())
+              s_aLogger.debug (sMsg, t);
+          }
+    }
+  }
+}
---
+++ tags/phloc-db-3.1.2/src/main/java/com/phloc/db/jpa/h2/H2DriverSingleton.java
@@ -0,0 +1,74 @@
+/**
+ * Copyright (C) 2006-2014 phloc systems
+ * http://www.phloc.com
+ * office[at]phloc[dot]com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.phloc.db.jpa.h2;
+
+import java.sql.Driver;
+
+import javax.annotation.Nonnull;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.phloc.commons.annotations.Nonempty;
+import com.phloc.commons.annotations.UsedViaReflection;
+import com.phloc.scopes.singleton.GlobalSingleton;
+
+/**
+ * A wrapper around the H2 driver, that gets automatically deregistered, when
+ * the global scope is closed.
+ * 
+ * @author Philip Helger
+ */
+public final class H2DriverSingleton extends GlobalSingleton
+{
+  private static final Logger s_aLogger = LoggerFactory.getLogger (H2DriverSingleton.class);
+
+  @Deprecated
+  @UsedViaReflection
+  public H2DriverSingleton ()
+  {
+    s_aLogger.info ("Loading org.h2.Driver");
+    org.h2.Driver.load ();
+  }
+
+  @Nonnull
+  public static H2DriverSingleton getInstance ()
+  {
+    return getGlobalSingleton (H2DriverSingleton.class);
+  }
+
+  @Nonnull
+  public Class <? extends Driver> getDriverClass ()
+  {
+    return org.h2.Driver.class;
+  }
+
+  @Nonnull
+  @Nonempty
+  public String getDriverClassName ()
+  {
+    return getDriverClass ().getName ();
+  }
+
+  @Override
+  protected void onDestroy () throws Exception
+  {
+    org.h2.Driver.unload ();
+    s_aLogger.info ("Unloaded org.h2.Driver");
+  }
+}
